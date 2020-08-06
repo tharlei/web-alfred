a28fcf88c51c3eea08ef6ee19106751b9ec4106a
Tharlei Aleixo <tharlei_aleixo@hotmail.com>
2020-08-06 19:52:15 -0300

    change api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://crudcrud.com/api/0597784c30db4de593d7497c439bea67'
+  baseURL: 'https://crudcrud.com/api/975f38f64a504e01beaec786e2c3e721'
 });
 
 export default api;
